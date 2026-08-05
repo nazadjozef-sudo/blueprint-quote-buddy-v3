65c39aa015c459de56b1bc377ffc39f2850b783c
nazadjozef-sudo <nazadjozef@gmail.com>
2026-08-06 01:27:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  root: './blueprint-quote-buddy-v3',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './blueprint-quote-buddy-v3/src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
