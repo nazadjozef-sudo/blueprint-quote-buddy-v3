fe70a8296edfce3676935eff7d8df26c03e877e2
nazadjozef-sudo <nazadjozef@gmail.com>
2026-08-06 02:59:12 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  root: process.cwd(),
   base: './',
   plugins: [react()],
   resolve: {
